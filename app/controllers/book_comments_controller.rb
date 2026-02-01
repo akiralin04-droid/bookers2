7b326a52c30e6faffd8ae15d8244798696e7b383
AkiraHayashi <akiralin04@gmail.com>
2026-02-01 02:15:48 +0000

    いいねとコメント機能を非同期化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,19 +1,20 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    # 投稿されたコメントの中に、ログインしているユーザーのIDも含める
+    @book = Book.find(params[:book_id])
+    # 投稿用の空の箱（フォームのリセット用）
+    @book_comment = BookComment.new
+    
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    # 前の画面に戻る
-    redirect_to book_path(book)
   end
 
   def destroy
     # URLから「どのコメントか」を探して削除
     # 他人のコメントを消せないよう、current_user.book_comments から探す
     BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    
+    @book = Book.find(params[:book_id])
   end
 
   private
